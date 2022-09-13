10025640c1b0a67794311e421a17b74cbfb86fc5
arslan-enes <arslanenes515@gmail.com>
2022-09-13 23:26:05 +0300

    wage transfer value datatype changes

--- todo.py
+++ todo.py
@@ -1,8 +1,6 @@
-#TODO Maaşları yıllık yap
 #TODO Transfer değerini modelle
-#todo Milli takım tecrübesi
 #TODO Maaş tahmini yap
-#TODO Parasal değişkenlerin formatı (milyon, bin vs.)
 #TODO Veri Tiplerini düzelt
 #TODO Liglere göre katsayı ekle
+#TODO Pozisyon sütünunu düzenle
 
