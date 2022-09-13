05936c2cda323225aee6226f4b9270719f31b653
yektaozan <yktaozn@gmail.com>
2022-09-14 02:34:06 +0300

    added weights of positions

---
+++ role_weights.py
@@ -0,0 +1,58 @@
+GK = {'Hea': 1, 'Tec': 1, 'Ant': 3, 'Bra': 6, 'Cmp': 2, 'Cnt': 6, 'Dec': 10,
+      'Ldr': 2, 'Pos': 5, 'Tea': 2, 'Vis': 1, 'Wor': 1, 'Acc': 6, 'Agi': 8,
+      'Bal': 2, 'Jum': 1, 'Pac': 3, 'Sta': 1, 'Str': 4, 'Aer': 6, 'Cmd': 6,
+      'Com': 5, 'Fir': 1, 'Han': 8, 'Kic': 5, '1v1': 4, 'Pas': 3, 'Ref': 8, 'Thr': 3}
+
+DRL = {'Cor': 1, 'Cro': 2, 'Dri': 1, 'Fin': 1, 'Fir': 3, 'Fre': 1, 'Hea': 2,
+      'Lon': 1, 'L th': 1, 'Mar': 3, 'Pas': 2, 'Pen': 1, 'Tck': 4, 'Tec': 2,
+      'Ant': 3, 'Bra': 2, 'Cmp': 2, 'Cnt': 4, 'Dec': 7, 'Ldr': 1, 'OtB': 1,
+      'Pos': 4, 'Tea': 2, 'Vis': 2, 'Wor': 2, 'Acc': 7, 'Agi': 6, 'Bal': 2,
+      'Jum': 2, 'Pac': 5, 'Sta': 6, 'Str': 4}
+
+DC = {'Cor': 1, 'Cro': 1, 'Dri': 1, 'Fin': 1, 'Fir': 2, 'Fre': 1, 'Hea': 5,
+      'Lon': 1, 'L th': 1, 'Mar': 8, 'Pas': 2, 'Pen': 1, 'Tck': 5, 'Tec': 1,
+      'Ant': 5, 'Bra': 2, 'Cmp': 2, 'Cnt': 4, 'Dec': 10, 'Ldr': 2, 'OtB': 1,
+      'Pos': 8, 'Tea': 1, 'Vis': 1, 'Wor': 2, 'Acc': 6, 'Agi': 6, 'Bal': 2,
+      'Jum': 6, 'Pac': 5, 'Sta': 3, 'Str': 6}
+
+WBRL = {'Cor': 1, 'Cro': 3, 'Dri': 2, 'Fin': 1, 'Fir': 3, 'Fre': 1, 'Hea': 1,
+        'Lon': 1, 'L th': 1, 'Mar': 2, 'Pas': 3, 'Pen': 1, 'Tck': 3, 'Tec': 3,
+        'Ant': 3, 'Bra': 1, 'Cmp': 2, 'Cnt': 3, 'Dec': 5, 'Ldr': 1, 'OtB': 2,
+        'Pos': 3, 'Tea': 2, 'Vis': 2, 'Wor': 2, 'Acc': 8, 'Agi': 5, 'Bal': 2,
+        'Jum': 1, 'Pac': 6, 'Sta': 7, 'Str': 4}
+
+DM = {'Cor': 1, 'Cro': 1, 'Dri': 2, 'Fin': 2, 'Fir': 4, 'Fre': 1, 'Hea': 1,
+      'Lon': 3, 'L th': 1, 'Mar': 3, 'Pas': 4, 'Pen': 1, 'Tck': 7, 'Tec': 3,
+      'Ant': 5, 'Bra': 1, 'Cmp': 2, 'Cnt': 3, 'Dec': 8, 'Ldr': 1, 'OtB': 1,
+      'Pos': 5, 'Tea': 2, 'Vis': 4, 'Wor': 4, 'Acc': 6, 'Agi': 6, 'Bal': 2,
+      'Jum': 1, 'Pac': 4, 'Sta': 4, 'Str': 5}
+
+MRL = {'Cor': 1, 'Cro': 5, 'Dri': 3, 'Fin': 2, 'Fir': 4, 'Fre': 1, 'Hea': 1,
+       'Lon': 2, 'L th': 1, 'Mar': 1, 'Pas': 3, 'Pen': 1, 'Tck': 2, 'Tec': 4,
+       'Ant': 3, 'Bra': 1, 'Cmp': 3, 'Cnt': 2, 'Dec': 5, 'Ldr': 1, 'OtB': 2,
+       'Pos': 1, 'Tea': 2, 'Vis': 3, 'Wor': 3, 'Acc': 8, 'Agi': 6, 'Bal': 2,
+       'Jum': 1, 'Pac': 6, 'Sta': 5, 'Str': 3}
+
+MC = {'Cor': 1, 'Cro': 1, 'Dri': 2, 'Fin': 2, 'Fir': 6, 'Fre': 1, 'Hea': 1,
+      'Lon': 3, 'L th': 1, 'Mar': 3, 'Pas': 6, 'Pen': 1, 'Tck': 3, 'Tec': 4,
+      'Ant': 3, 'Bra': 1, 'Cmp': 3, 'Cnt': 2, 'Dec': 7, 'Ldr': 1, 'OtB': 3,
+      'Pos': 3, 'Tea': 2, 'Vis': 6, 'Wor': 3, 'Acc': 6, 'Agi': 6, 'Bal': 2,
+      'Jum': 1, 'Pac': 5, 'Sta': 6, 'Str': 4}
+
+AMRL = {'Cor': 1, 'Cro': 5, 'Dri': 5, 'Fin': 2, 'Fir': 5, 'Fre': 1, 'Hea': 1,
+        'Lon': 2, 'L th': 1, 'Mar': 1, 'Pas': 2, 'Pen': 1, 'Tck': 2, 'Tec': 4,
+        'Ant': 3, 'Bra': 1, 'Cmp': 3, 'Cnt': 2, 'Dec': 5, 'Ldr': 1, 'OtB': 2,
+        'Pos': 1, 'Tea': 2, 'Vis': 3, 'Wor': 3, 'Acc': 10, 'Agi': 6, 'Bal': 2,
+        'Jum': 1, 'Pac': 10, 'Sta': 7, 'Str': 3}
+
+AMC = {'Cor': 1, 'Cro': 1, 'Dri': 3, 'Fin': 3, 'Fir': 5, 'Fre': 1, 'Hea': 1,
+       'Lon': 3, 'L th': 1, 'Mar': 1, 'Pas': 4, 'Pen': 1, 'Tck': 2, 'Tec': 5,
+       'Ant': 3, 'Bra': 1, 'Cmp': 3, 'Cnt': 2, 'Dec': 6, 'Ldr': 1, 'OtB': 3,
+       'Pos': 2, 'Tea': 2, 'Vis': 6, 'Wor': 3, 'Acc': 9, 'Agi': 6, 'Bal': 2,
+       'Jum': 1, 'Pac': 7, 'Sta': 6, 'Str': 3}
+
+SC = {'Cor': 1, 'Cro': 2, 'Dri': 5, 'Fin': 8, 'Fir': 6, 'Fre': 1, 'Hea': 6,
+      'Lon': 2, 'L th': 1, 'Mar': 1, 'Pas': 2, 'Pen': 1, 'Tck': 1, 'Tec': 4,
+      'Ant': 5, 'Bra': 1, 'Cmp': 6, 'Cnt': 2, 'Dec': 5, 'Ldr': 1, 'OtB': 6,
+      'Pos': 2, 'Tea': 1, 'Vis': 2, 'Wor': 2, 'Acc': 10, 'Agi': 6, 'Bal': 2,
+      'Jum': 5, 'Pac': 7, 'Sta': 6, 'Str': 6}
