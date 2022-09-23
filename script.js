e7fb1daa190ffe84a8d9f0e02053b27b9159989f
arslan-enes <arslanenes515@gmail.com>
2022-09-23 11:40:35 +0300

    sidebar and buttons fix2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         const doc = window.parent.document;
-        doc.querySelector('.css-1qrvfrg').addEventListener('click', function() {
+        doc.querySelector('.css-1prua9e').addEventListener('click', function() {
         doc.querySelectorAll('.css-9s5bis')[1].click(); // same behavior as doc.querySelector('[role="combobox"]').click()
         });
     }
